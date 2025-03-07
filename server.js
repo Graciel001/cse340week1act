889d0d9995d30a3d6b8e9a52636d99e3e8d8ca42
Benjamin <gracielvelazquez464@gmail.com>
2025-03-07 11:12:04 -0700

    benjamin

--- server.js
+++ server.js
@@ -6,14 +6,35 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+const path = require("path"); // Importar módulo para manejar rutas
+
+// Configurar la carpeta de vistas correctamente
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+
 /* ***********************
  * Routes
  *************************/
+
+// Index Route
+
 app.use(static)
+
+app.get("/", function(req, res){
+  res.render("index", {title: "home"})
+})
 
 /* ***********************
  * Local Server Information
@@ -21,6 +42,8 @@ app.use(static)
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
+const DATABASE_URL = process.env.DATABASE_URL
+
 
 /* ***********************
  * Log statement to confirm server operation
